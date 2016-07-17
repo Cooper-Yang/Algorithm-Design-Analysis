63bc9f2eda962b2ec26a50fc46d0b38ff3511037
Cooper Yang <misairu.gugu@gmail.com>
2016-07-18 01:17:32 +0800

    bug fixed

--- CountingInversions.py
+++ CountingInversions.py
@@ -5,30 +5,46 @@ Counting Inversions Algorithm
 
 import sys
 import os
+from copy import deepcopy
 
 def counting_inversions(input_array):
 	"""
 	counting inversions using merge sort algorithm in nlog(n) time
-	:type argv: list
+	:type input_array: list
 	"""
 	number_of_inversions = 0
 	if len(input_array) <= 1:
 		return number_of_inversions
-	left_array = input_array[0:len(input_array)/2]
-	right_array = input_array[len(input_array)/2:len(input_array)]
+	left_array = input_array[:len(input_array)/2]
+	right_array = input_array[len(input_array)/2:]
 	number_of_inversions += counting_inversions(left_array)
 	number_of_inversions += counting_inversions(right_array)
+	temp_i = 0
 	j = 0
 	k = 0
 	try:
-		for _ in range(0, len(input_array)):
-			if left_array[j] < right_array[k]:
+		for i in range(0, len(input_array)):
+			if int(left_array[j]) <= int(right_array[k]):
+				input_array[i] = left_array[j]
 				j += 1
-			if left_array[j] > right_array[k]:
-				k += 1
+				temp_i = i+1
+				continue
+			if int(left_array[j]) > int(right_array[k]):
+				input_array[i] = right_array[k]
 				number_of_inversions += (len(left_array) - j)
+				k += 1
+				temp_i = i+1
+				continue
 	except IndexError:
-		pass
+		if j >= len(left_array):
+			for i in range(temp_i, len(input_array)):
+				input_array[i] = right_array[k]
+				k += 1
+		else:
+			# k >= len(right_array)
+			for i in range(temp_i, len(input_array)):
+				input_array[i] = left_array[j]
+				j += 1
 	return number_of_inversions
 
 def main_func(input_argv):
